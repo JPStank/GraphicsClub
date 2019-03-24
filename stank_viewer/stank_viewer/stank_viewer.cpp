65294b9b7d50cfeb3b3002d0065611c3ae728e04
Josh S <naana.policy@gmail.com>
2019-03-23 22:28:39 -0500

    initd3d done

--- stank_viewer/stank_viewer/stank_viewer.cpp
+++ stank_viewer/stank_viewer/stank_viewer.cpp
@@ -23,8 +23,8 @@ void mainloop();
 
 
 // Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
+//ATOM                MyRegisterClass(HINSTANCE hInstance);
+//BOOL                InitInstance(HINSTANCE, int);
 //LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
@@ -44,8 +44,20 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		return 0;
 	}
 
+	if (!InitD3D())
+	{
+		MessageBox(0, L"Failed to initialize direct3d 12",
+			L"ERROR", MB_OK);
+		Cleanup();
+		return 1;
+	}
+
 	mainloop();
 
+	// need to wait for GPU to finish executing
+	WaitForPreviousFrame();
+
+	CloseHandle(fenceEvent);
 
     return 0;
 }
@@ -154,4 +166,165 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
+}
+
+
+bool InitD3D()
+{
+	HRESULT hr;
+
+	IDXGIFactory4* dxgiFactory;
+	hr = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
+	if (FAILED(hr))
+	{
+		return false;
+	}
+
+	IDXGIAdapter1* adapter;
+
+	int adapterIndex = 0;
+
+	bool adapterFound = false;
+
+	// walk the adapters, break on first compatilble with feature level 11
+	while (dxgiFactory->EnumAdapters1(adapterIndex, &adapter) != DXGI_ERROR_NOT_FOUND)
+	{
+		DXGI_ADAPTER_DESC1 desc;
+		adapter->GetDesc1(&desc);
+
+		if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+		{
+			adapterIndex++;
+			continue;
+		}
+
+		hr = D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr);
+		if (SUCCEEDED(hr))
+		{
+			adapterFound = true;
+			break;
+		}
+
+		adapterIndex++;
+	}
+
+	if (!adapterFound)
+	{
+		return false;
+	}
+
+	// make the device
+	hr = D3D12CreateDevice(
+		adapter,
+		D3D_FEATURE_LEVEL_11_0,
+		IID_PPV_ARGS(&device));
+
+	if (FAILED(hr))
+	{
+		return false;
+	}
+
+	// create command queue
+	D3D12_COMMAND_QUEUE_DESC cqDesc = {}; //use all defaults
+
+	hr = device->CreateCommandQueue(&cqDesc, IID_PPV_ARGS(&commandQueue));
+	if (FAILED(hr))
+	{
+		return false;
+	}
+
+	// create swap chain
+	DXGI_MODE_DESC backBufferDesc = {};
+	backBufferDesc.Width = width;
+	backBufferDesc.Height = height;
+	backBufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+	DXGI_SAMPLE_DESC sampleDesc = {};
+	sampleDesc.Count = 1;
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+	swapChainDesc.BufferCount = frameBufferCount;
+	swapChainDesc.BufferDesc = backBufferDesc;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swapChainDesc.OutputWindow = hwnd;
+	swapChainDesc.SampleDesc = sampleDesc;
+	swapChainDesc.Windowed = !fullscreen;
+
+	IDXGISwapChain* tempSwapChain;
+
+	dxgiFactory->CreateSwapChain(commandQueue, &swapChainDesc, &tempSwapChain);
+
+	swapChain = static_cast<IDXGISwapChain3*>(tempSwapChain);
+
+	frameIndex = swapChain->GetCurrentBackBufferIndex();
+
+	//descriptor heap
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+	rtvHeapDesc.NumDescriptors = frameBufferCount;
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+
+	// not gonna be references by shaders, only store pipeline output
+	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	hr = device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvDescriptorHeap));
+	if (FAILED(hr))
+	{
+		return false;
+	}
+
+	rtvDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart());
+
+	// create rtv for each buffer
+	for (int i = 0; i < frameBufferCount; ++i)
+	{
+		hr = swapChain->GetBuffer(i, IID_PPV_ARGS(&renderTargets[i]));
+		if (FAILED(hr))
+		{
+			return false;
+		}
+
+		device->CreateRenderTargetView(renderTargets[i], nullptr, rtvHandle);
+
+		rtvHandle.Offset(1, rtvDescriptorSize);
+	}
+
+	//command allocator
+	for (int i = 0; i < frameBufferCount; i++)
+	{
+		hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator[i]));
+		if (FAILED(hr))
+		{
+			return false;
+		}
+	}
+
+	//command list
+	hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator[0], NULL, IID_PPV_ARGS(&commandList));
+	if (FAILED(hr))
+	{
+		return false;
+	}
+
+	commandList->Close();
+
+	//fences and fence events
+	for (int i = 0; i < frameBufferCount; i++)
+	{
+		hr = device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence[i]));
+		if (FAILED(hr))
+		{
+			return false;
+		}
+		fenceValue[i] = 0;
+	}
+
+	fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+	if (fenceEvent == nullptr)
+	{
+		return false;
+	}
+
+	return true;
 }
--- stank_viewer/stank_viewer/stdafx.h
+++ stank_viewer/stank_viewer/stdafx.h
@@ -18,3 +18,45 @@
 #include "d3dx12.h"
 
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// d3d stuff
+const int frameBufferCount = 3;
+
+ID3D12Device* device;
+
+IDXGISwapChain3* swapChain;
+
+ID3D12CommandQueue* commandQueue;
+
+ID3D12DescriptorHeap* rtvDescriptorHeap;
+
+ID3D12Resource* renderTargets[frameBufferCount];
+
+ID3D12CommandAllocator* commandAllocator[frameBufferCount];
+
+ID3D12GraphicsCommandList* commandList;
+
+ID3D12Fence* fence[frameBufferCount];
+
+HANDLE fenceEvent;
+
+UINT64 fenceValue[frameBufferCount];
+
+int frameIndex;
+
+int rtvDescriptorSize;
+
+// function declarations
+
+bool InitD3D();
+
+void Update();
+
+void UpdatePipeline();
+
+void Render();
+
+void Cleanup();
+
+void WaitForPreviousFrame();
+
